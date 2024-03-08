47687c78d2de5a3b82570e4527a1987cebfb18d0
Claudius Mbemba <mbembac@gmail.com>
2024-03-07 19:48:19 -0800

    build

--- dist/agent.js
+++ dist/agent.js
@@ -120,6 +120,24 @@ var tryParse = (jsonString) => {
     return false;
   }
 };
+var askAiForAnswer = async (openai2, messages) => {
+  messages = messages?.length <= 4 ? messages : [...messages.slice(0, 2), ...messages.slice(-1)];
+  console.log("Messages:", messages?.length);
+  const response = await openai2.chat.completions.create({
+    model: "gpt-4-vision-preview",
+    max_tokens: 1024,
+    // max_tokens: 100, //model's response will be limited to 100 tokens.
+    messages
+    // G2: send system message and last 2 messages (turns)
+  });
+  const message = response.choices[0].message;
+  const message_text = (message?.content || "").trim();
+  messages.push({
+    "role": "assistant",
+    "content": message_text
+  });
+  return message_text;
+};
 
 // agent.ts
 dotenv.config();
@@ -143,47 +161,8 @@ var systemMessage = {
 
     Use google search by set a sub-page like 'https://google.com/search?q=search' if applicable. Prefer to use Google for simple queries. If the user provides a direct URL, go to that one. Do not make up links`
 };
-var messages = [];
-var scrollAndScreenshot = async (page) => {
-  const totalHeight = await page.evaluate(() => document.body.scrollHeight);
-  let currentHeight = 0, previousHeight = 0, canScroll = true, lastResponse = "";
-  await highlight_links(page);
-  while (currentHeight < totalHeight) {
-    previousHeight = currentHeight;
-    await page.evaluate(() => window.scrollBy(0, window.innerHeight));
-    await page.waitForTimeout(1e3);
-    currentHeight = await page.evaluate(() => window.scrollY + window.innerHeight);
-    if (currentHeight > previousHeight) {
-      await page.screenshot({
-        // // will capture a 150x100 pixel area starting from the point (50, 100) on the webpage 
-        // clip: {
-        //     x: 50, // X coordinate of the top-left corner of the area
-        //     y: 100, // Y coordinate of the top-left corner of the area
-        //     width: 150, // Width of the area to capture
-        //     height: 100 // Height of the area to capture
-        // },
-        path: "screenshot.jpg",
-        quality: 100
-        // fullPage: true,
-      });
-      console.log(`Took screenshot at height: ${currentHeight}`);
-      await addScreenshotMessage();
-      lastResponse = await askAiForAnswer();
-      const response = tryParse(lastResponse);
-      if (response && response?.scroll) {
-        canScroll = true;
-      }
-    }
-    if (currentHeight >= totalHeight && lastResponse.includes("scroll")) {
-      messages.push({
-        "role": "assistant",
-        //"user"
-        "content": "ERROR: I was unable to find the answer to your question. Please try asking again or rephrasing your question."
-      });
-    }
-  }
-};
-var addScreenshotMessage = async () => {
+var resolution = 2;
+var addScreenshotMessage = async (messages) => {
   const base64_image = await image_to_base64("screenshot.jpg");
   messages.push({
     "role": "user",
@@ -199,44 +178,42 @@ var addScreenshotMessage = async () => {
     ])
   });
 };
-var browsePageForAnswer = async (page, url) => {
-  if (typeof url === "string" && url.length > 0) {
-    console.log("Crawling " + url);
-    await page.goto(url, {
-      waitUntil: "domcontentloaded",
-      timeout
-    });
-    await Promise.race([
-      waitForEvent(page, "load"),
-      sleep(timeout)
-    ]);
-  }
-  await scrollAndScreenshot(page);
+var handleProcessUrl = async (aiResponse) => {
+  const resp = tryParse(aiResponse);
+  return resp ? resp?.url : null;
 };
-var askAiForAnswer = async () => {
-  messages = messages?.length <= 4 ? messages : [...messages.slice(0, 2), ...messages.slice(-2)];
-  console.log("Messages:", JSON.stringify(messages, null, 2));
-  const response = await openai.chat.completions.create({
-    model: "gpt-4-vision-preview",
-    max_tokens: 1024,
-    // max_tokens: 100, //model's response will be limited to 100 tokens.
-    messages
-    // G2: send system message and last 2 messages (turns)
-  });
-  const message = response.choices[0].message;
-  const message_text = (message?.content || "").trim();
-  messages.push({
-    "role": "assistant",
-    "content": message_text
-  });
-  return message_text;
+var handleClickLink = async (page, aiResponse) => {
+  const resp = tryParse(aiResponse);
+  if (resp) {
+    const link = resp.click;
+    console.log("Clicking on " + link);
+    const elements = await page.$$("[gpt-link-text]");
+    let partial, exact;
+    for (const element of elements) {
+      const attributeValue = await element.evaluate((el) => el.getAttribute("gpt-link-text"));
+      if (attributeValue?.includes(link)) {
+        partial = element;
+      }
+      if (attributeValue === link) {
+        exact = element;
+      }
+    }
+    if (exact || partial) {
+      const [response] = await Promise.all([
+        page.waitForNavigation({ waitUntil: "domcontentloaded" }).catch((e) => console.log("Navigation timeout/error:", e.message)),
+        (exact || partial).click()
+      ]);
+    }
+  }
+  throw new Error("Can't find link");
 };
 var runAgent = async (prompt, options) => {
   console.log("###########################################");
   console.log("# GPT4V-Browsing by Unconventional Coding #");
   console.log("###########################################\n");
-  let url, agent_done = false, messages2 = [systemMessage];
-  messages2.push({
+  let currentHeight = 0, previousHeight = 0, scrollFactor = 7, screenShotCounter = 0, scrollCounter = 0;
+  let agentDone = false, messages = [systemMessage];
+  messages.push({
     "role": "user",
     "content": prompt
   });
@@ -252,60 +229,103 @@ var runAgent = async (prompt, options) => {
       console.log(await msgArgs[i].jsonValue());
     }
   });
-  await page.setViewport({
-    width: 1200,
-    height: 1200,
-    deviceScaleFactor: 1
-  });
-  while (!agent_done) {
-    if (url) {
-      await browsePageForAnswer(page, url);
-    }
-    const aiResponse = await askAiForAnswer();
-    if (aiResponse.includes("click")) {
-      const resp = tryParse(aiResponse);
-      if (resp) {
-        const link = resp.click;
-        console.log("Clicking on " + link);
-        try {
-          const elements = await page.$$("[gpt-link-text]");
-          let partial, exact;
-          for (const element of elements) {
-            const attributeValue = await element.evaluate((el) => el.getAttribute("gpt-link-text"));
-            if (attributeValue?.includes(link)) {
-              partial = element;
-            }
-            if (attributeValue === link) {
-              exact = element;
-            }
-          }
-          if (exact || partial) {
-            const [response] = await Promise.all([
-              page.waitForNavigation({ waitUntil: "domcontentloaded" }).catch((e) => console.log("Navigation timeout/error:", e.message)),
-              (exact || partial).click()
-            ]);
-            url = true;
-          } else {
-            throw new Error("Can't find link");
-          }
-        } catch (error) {
-          console.log("ERROR: Clicking failed", error);
-          messages2.push({
-            "role": "assistant",
-            //"user"
-            "content": "ERROR: I was unable to click that element"
-          });
+  await page.setViewport({ width: 1200, height: 1200, deviceScaleFactor: resolution });
+  let oldLayout;
+  while (!agentDone) {
+    const currentLayout = await page.evaluate(() => {
+      return {
+        top: window.screenTop,
+        left: window.screenLeft,
+        windowH: window.innerHeight,
+        windowW: window.innerWidth,
+        totalH: document.body.scrollHeight
+      };
+    });
+    console.log("Current URL:", page.url());
+    const totalHeight = await page.evaluate(() => document.body.scrollHeight);
+    const aiResponse = await askAiForAnswer(openai, messages);
+    console.log("AI Response:", aiResponse);
+    if (aiResponse.includes("scroll")) {
+      if (screenShotCounter === scrollFactor) {
+        screenShotCounter = 0;
+        scrollCounter++;
+        previousHeight = currentHeight;
+        await page.evaluate(() => window.scrollBy(0, window.innerHeight));
+        await page.waitForTimeout(1e3);
+        currentHeight = await page.evaluate((scrollFactor2) => window.scrollY + window.innerHeight / scrollFactor2, scrollFactor);
+        if (currentHeight >= totalHeight) {
+          agentDone = true;
         }
       }
-      url = null;
-    } else if (aiResponse.includes("url")) {
-      const resp = tryParse(aiResponse);
-      url = resp ? resp?.url : null;
     }
-    if (!url) {
-      agent_done = true;
-      prompt = "";
-      console.log("Agent done...");
+    if (aiResponse.includes("url")) {
+      oldLayout = null;
+      previousHeight = 0, currentHeight = 0;
+      const url = await handleProcessUrl(aiResponse);
+      if (url) {
+        console.log("Going to " + url);
+        await page.goto(url, {
+          waitUntil: "domcontentloaded",
+          timeout
+        });
+        await Promise.race([
+          waitForEvent(page, "load"),
+          sleep(timeout)
+        ]);
+      } else {
+        agentDone = true;
+      }
+    }
+    if (aiResponse.includes("click")) {
+      oldLayout = null;
+      previousHeight = 0, currentHeight = 0;
+      try {
+        await handleClickLink(page, aiResponse);
+      } catch (error) {
+        agentDone = true;
+        console.log("ERROR: Clicking failed", error);
+        messages.push({
+          "role": "assistant",
+          //"user"
+          "content": "ERROR: I was unable to click that element"
+        });
+      }
+    }
+    if (agentDone) {
+      messages.push({
+        "role": "assistant",
+        //"user"
+        "content": "ERROR: I was unable to find the answer to your question. Please try asking again or rephrasing your question."
+      });
+      continue;
+    }
+    if (currentHeight == 0) {
+      await highlight_links(page);
+    }
+    const screenshotH = currentLayout?.windowH / scrollFactor;
+    const clip = {
+      // // will capture a 150x100 pixel area starting from the point (50, 100) on the webpage 
+      top: currentLayout?.top,
+      // X coordinate of the top-left corner of the area
+      left: oldLayout ? oldLayout?.left + screenshotH : currentLayout?.left,
+      // Y coordinate of the top-left corner of the area
+      width: currentLayout?.windowW,
+      // Width of the area to capture
+      height: screenshotH
+      // Height of the area to capture
+    };
+    console.log(`Taking screenshot at`, clip);
+    console.log(`scrollCounter:${scrollCounter}, screenShotCounter:${screenShotCounter}`);
+    await page.screenshot({
+      clip: { ...clip, x: clip?.top, y: clip?.left },
+      path: "screenshot.jpg",
+      quality: 100
+      // fullPage: true,
+    });
+    await addScreenshotMessage(messages);
+    screenShotCounter++;
+    if (screenShotCounter !== scrollFactor) {
+      oldLayout = clip;
     }
   }
 };
